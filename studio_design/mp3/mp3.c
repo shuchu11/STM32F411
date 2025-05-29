9df98b8c268cca7f43d766039ae37da79ad83530
SHU_CHI <163873199+shuchu11@users.noreply.github.com>
2025-05-29 13:45:03 +0800

    Create mp3.c

---
+++ studio_design/mp3/mp3.c
@@ -0,0 +1,17 @@
+void play_mp3_stream(const char* filename)
+{
+    if (mp3dec_ex_open(&dec, filename, MP3D_SEEK_TO_SAMPLE) != 0) {
+        Error_Handler();  // 無法開啟或格式錯誤
+    }
+
+    while (1) {
+        int samples = mp3dec_ex_read(&dec, pcm_buffer, PCM_BUFFER_SIZE);
+        if (samples <= 0) break;
+
+        // stereo: 每個樣本包含 L+R，因此 word 數量 = samples × channels
+        HAL_I2S_Transmit_DMA(&hi2s3, (uint16_t*)pcm_buffer, samples);
+        while (HAL_I2S_GetState(&hi2s3) != HAL_I2S_STATE_READY);
+    }
+
+    mp3dec_ex_close(&dec);
+}
